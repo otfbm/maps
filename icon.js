df8834e9fc487d3ca48e301faa2d7c368404111c
Richard Walker <digitalsadhu@gmail.com>
2020-05-22 18:49:17 +1200

    add icon parser

--- icon.js
+++ icon.js
@@ -2,11 +2,6 @@ import canvas from "canvas";
 
 const { Image } = canvas;
 
-const icons = new Map([
-  ["$D", "./icons/doorway.svg"],
-  ["$D!", "./icons/doorway.png"],
-]);
-
 export default class Icon {
   constructor({ name } = {}) {
     this.name = name;
---
+++ test/parsers/icon.js
@@ -0,0 +1,34 @@
+import tap from 'tap';
+import IconParser from '../../parsers/icon.js';
+
+const { test } = tap;
+
+test('parsing: 1', (t) => {
+    const parser = new IconParser();
+    const { icon, x, y } = parser.parse('C3$D');
+    
+    t.equal(x, 3);
+    t.equal(y, 3);
+    t.same(icon, './icons/doorway.svg');
+    t.end();
+});
+
+test('parsing: 2', (t) => {
+    const parser = new IconParser();
+    const { icon, x, y } = parser.parse('/E3$D');
+    
+    t.equal(x, 5);
+    t.equal(y, 3);
+    t.same(icon, './icons/doorway.svg');
+    t.end();
+});
+
+test('parsing: 2', (t) => {
+    const parser = new IconParser();
+    const { icon, x, y } = parser.parse('/F3$D/');
+    
+    t.equal(x, 6);
+    t.equal(y, 3);
+    t.same(icon, './icons/doorway.svg');
+    t.end();
+});
