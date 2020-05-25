8931a00ec617460ebd6da73aa659bcdfcbd07528
Richard Walker <digitalsadhu@gmail.com>
2020-05-25 21:50:44 +1200

    use throng

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-import cluster from 'cluster';
+import throng from 'throng';
 import os from 'os';
 import createServer from './server.js';
 
 const numCPUs = os.cpus().length;
+const WORKERS = process.env.WEB_CONCURRENCY || numCPUs;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', worker => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, () => {
   const server = createServer();
   server.listen(process.env.PORT || 3000, "0.0.0.0");
-}
+});
