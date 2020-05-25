ea2996d2b3270941285d355f5c4f78c4b7f4f656
Richard Walker <digitalsadhu@gmail.com>
2020-05-26 10:55:20 +1200

    use jpeg to reduce download sizes

--- server.js
+++ server.js
@@ -38,11 +38,11 @@ export default function createServer() {
     }
 
     const canvas = drawCanvas(request.params["*"], backgroundImage);
-    const data = canvas.toDataURL("image/png", 1);
+    const data = canvas.toDataURL("image/jpeg", { quality: 1 });
     const stripped = data.replace(/^data:image\/\w+;base64,/, "");
     const buff = new Buffer(stripped, "base64");
 
-    reply.type("image/png").send(buff);
+    reply.type("image/jpeg").send(buff);
   });
 
   return server;
