b14cd3284646047fce85a1648cf87e799a999479
Richard Walker <digitalsadhu@gmail.com>
2020-06-14 19:52:41 +1200

    fix token parser tests

--- test/parsers/token.mjs
+++ test/parsers/token.mjs
@@ -5,96 +5,80 @@ const { test } = tap;
 
 test('parsing: position 1', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('/D3');
+    const { color, size, label } = parser.parse('/D3');
     
-    t.equal(x, 4);
-    t.equal(y, 3);
     t.same(color, 'black');
     t.same(size, 'medium');
-    t.equal(name, '');
+    t.equal(label, '');
     t.end();
 });
 
 test('parsing: position 2', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('D3');
+    const { color, size, label } = parser.parse('D3');
     
-    t.equal(x, 4);
-    t.equal(y, 3);
     t.same(color, 'black');
     t.same(size, 'medium');
-    t.equal(name, '');
+    t.equal(label, '');
     t.end();
 });
 
 test('parsing: position 3', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('D12');
+    const { color, size, label } = parser.parse('D12');
     
-    t.equal(x, 4);
-    t.equal(y, 12);
     t.same(color, 'black');
     t.same(size, 'medium');
-    t.equal(name, '');
+    t.equal(label, '');
     t.end();
 });
 
 test('parsing: position and 1 flag', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('/D3r');
+    const { color, size, label } = parser.parse('/D3r');
     
-    t.equal(x, 4);
-    t.equal(y, 3);
     t.same(color, 'firebrick');
     t.same(size, 'medium');
-    t.equal(name, '');
+    t.equal(label, '');
     t.end();
 });
 
 test('parsing: position and 2 flags', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('/D3rL');
+    const { color, size, label } = parser.parse('/D3rL');
     
-    t.equal(x, 4);
-    t.equal(y, 3);
     t.same(color, 'firebrick');
     t.same(size, 'large');
-    t.equal(name, '');
+    t.equal(label, '');
     t.end();
 });
 
 test('parsing: position and name', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('/D3-name');
+    const { color, size, label } = parser.parse('/D3-name');
     
-    t.equal(x, 4);
-    t.equal(y, 3);
     t.same(color, 'black');
     t.same(size, 'medium');
-    t.equal(name, 'name');
+    t.equal(label, 'name');
     t.end();
 });
 
 test('parsing: position, 1 flag and name', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('/D3r-name');
+    const { color, size, label } = parser.parse('/D3r-name');
     
-    t.equal(x, 4);
-    t.equal(y, 3);
     t.same(color, 'firebrick');
     t.same(size, 'medium');
-    t.equal(name, 'name');
+    t.equal(label, 'name');
     t.end();
 });
 
 test('parsing: position, 2 flags and name', (t) => {
     const parser = new TokenParser();
-    const { x, y, color, size, name } = parser.parse('/D3rL-name');
+    const { color, size, label } = parser.parse('/D3rL-name');
     
-    t.equal(x, 4);
-    t.equal(y, 3);
     t.same(color, 'firebrick');
     t.same(size, 'large');
-    t.equal(name, 'name');
+    t.equal(label, 'name');
     t.end();
 });
