6fbf0d797f871d78a4f69bd15d37dbf316ae876b
Matt <matthew.spence@gmail.com>
2020-08-03 19:50:20 +1200

    zoom parser format changed to x.xxx

--- parsers/zoom.js
+++ parsers/zoom.js
@@ -1,31 +1,12 @@
-const minZoom = 1;
-const maxZoom = 5;
-
 module.exports = class ZoomParser {
     parse(str) {
         let trimmed = str.trim();
         if (trimmed[0] === '/') trimmed = trimmed.substr(1);
         if (trimmed[trimmed.length-1] === '/') trimmed = trimmed.substr(0, trimmed.length - 1);
 
-        /* If we've gotten this far, we can assume any number provided
-           is the zoom level we're hoping for */
-        for (var i = minZoom; i <= maxZoom; i++) {
-            let re = new RegExp('^@.*[' + i + '].*$');
-            if (re.test(trimmed)) {
-                switch (i) {
-                    case 1:
-                        return 0.5;
-                    case 2:
-                        return 0.75;
-                    case 3:
-                        return 1;
-                    case 4:
-                        return 1.5;
-                    case 5:
-                        return 2;
-                }
-            }
-        }
+        let match = trimmed.match(/[0-9](.[0-9]{1,3})?/);
+        if (match)
+            return Number(match[0]);
 
         return false;
     }
