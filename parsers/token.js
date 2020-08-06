cda5cbb47ea41de4a3d786720feff2352a25d626
Matt <matthew.spence@gmail.com>
2020-08-06 12:44:10 +1200

    wee tidy up

--- parsers/token.js
+++ parsers/token.js
@@ -21,7 +21,7 @@ module.exports = class TokenParser {
     if (str.length < 2) return false;
 
     // a string matching a token definition eg. D3rp-asdsa
-    const reg = /^([A-Z]{1,4}[0-9]{1,2})([TSMLHG]*?)(BK|GY|BN|[WKEARGBYPCNO]|~[0-9A-f]{6}|~[0-9A-f]{3})?(-(.+))?$/i;
+    const reg = /^([A-Z]{1,2}[0-9]{1,2})([TSMLHG]*?)(BK|GY|BN|[WKEARGBYPCNO]|~[0-9A-f]{6}|~[0-9A-f]{3})?(-(.+))?$/i;
     if (reg.test(trimmed)) {
       const matches = trimmed.match(reg);
 
