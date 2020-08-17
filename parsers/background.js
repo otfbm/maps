6b50cd422aea6f26ac294948ed5b5c14046c6d92
Richard Walker <digitalsadhu@gmail.com>
2020-08-17 12:03:50 +0200

    use URL class to build URLs

--- parsers/background.js
+++ parsers/background.js
@@ -1,28 +1,27 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = class BackgroundParser {
-    async parse(query) {
-        let backgroundImage = null;
-        if (query.bg) {
-            const bgendpoint = "https://bg.otfbm.io/";
-            const bgurl = bgendpoint + Buffer.from(query.bg).toString('base64');
-            const res = await fetch(bgurl);
+  async parse(query) {
+    let backgroundImage = null;
+    if (query.bg) {
+      const pathname = Buffer.from(query.bg).toString("base64");
+      const bgBaseURL = "https://bg.otfbm.io";
+      const res = await fetch(new URL(pathname, bgBaseURL));
 
-            const contentLength = res.headers.get('content-length');
-            if (contentLength > 1102000) {
-                res.destroy();
-                throw new Error('Background too large');
-            }
-            
-            const buffer = await res.buffer();
+      const contentLength = res.headers.get("content-length");
+      if (contentLength > 1102000) {
+        res.destroy();
+        throw new Error("Background too large");
+      }
 
-            if (buffer.byteLength > 1102000)
-                throw new Error('Background too large');
+      const buffer = await res.buffer();
 
-            backgroundImage = `data:${
-                res.headers.get("content-type")
-            };base64,${buffer.toString("base64")}`;
-        }      
-        return backgroundImage;
+      if (buffer.byteLength > 1102000) throw new Error("Background too large");
+
+      backgroundImage = `data:${res.headers.get(
+        "content-type"
+      )};base64,${buffer.toString("base64")}`;
     }
-}
+    return backgroundImage;
+  }
+};
