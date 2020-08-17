d68865a7ccef1109800058bb143d4ca86dad2eb9
Thrombo <626446+RCarretta@users.noreply.github.com>
2020-08-17 03:06:41 -0700

    integrate preload into bg fetch (#6)
    * integrate preload into bg fetch
    
    * slightly increase 1mb bg image limit and throw errors when bg too large
    
    * use URL class to build URLs
    
    Co-authored-by: Richard Walker <digitalsadhu@gmail.com>

--- parsers/background.js
+++ parsers/background.js
@@ -1,30 +1,27 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = class BackgroundParser {
-    async parse(query) {
-        let backgroundImage = null;
-        if (query.bg) {
-            try {
-                const res = await fetch(query.bg);
+  async parse(query) {
+    let backgroundImage = null;
+    if (query.bg) {
+      const pathname = Buffer.from(query.bg).toString("base64");
+      const bgBaseURL = "https://bg.otfbm.io";
+      const res = await fetch(new URL(pathname, bgBaseURL));
 
-                const contentLength = res.headers.get('content-length');
-                if (contentLength > 1000000) {
-                    res.destroy();
-                    throw new Error('Background too large');
-                }
-                
-                const buffer = await res.buffer();
+      const contentLength = res.headers.get("content-length");
+      if (contentLength > 1102000) {
+        res.destroy();
+        throw new Error("Background too large");
+      }
 
-                if (buffer.byteLength > 1000000)
-                    throw new Error('Background too large');
+      const buffer = await res.buffer();
 
-                backgroundImage = `data:${
-                    res.headers.get("content-type")
-                };base64,${buffer.toString("base64")}`;
-            } catch (err) {
-                // noop
-            }
-        }      
-        return backgroundImage;
+      if (buffer.byteLength > 1102000) throw new Error("Background too large");
+
+      backgroundImage = `data:${res.headers.get(
+        "content-type"
+      )};base64,${buffer.toString("base64")}`;
     }
-}
+    return backgroundImage;
+  }
+};
