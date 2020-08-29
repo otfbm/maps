17bc862619ffc7456e678b1ee66d67314d8ac614
Ryan Carretta <626446+RCarretta@users.noreply.github.com>
2020-08-29 16:18:30 -0700

    properly upload metadata file and jpg to s3

--- token/otfbm-token.py
+++ token/otfbm-token.py
@@ -42,17 +42,17 @@ def lambda_handler(event, context):
             },
             ReturnValues="UPDATED_NEW"
         )
-        number = int(response['Attributes']['token_counter']) # this is the number we have to shorturl
-        shortCode = short_url.encode(number)
+        number = int(response['Attributes']['token_counter'])  # this is the number we have to shorturl
+        shortCode = short_url.encode_url(number)
 
-        contents = f'<html><head><title>Token ID for {imgUrl}</title><meta name="description" content="{shortCode}"></head><body>{shortCode}</body></html>'
+        contents = f'<html><head><title>Token ID for {imgUrl.decode("utf-8")}</title><meta name="description" content="{shortCode}"></head><body>{shortCode}</body></html>'
         body = contents.encode()
 
         s3_client.put_object(Body=body, Bucket=bucket, Key=f'meta/{imgUrlEnc}',
-                             ExtraArgs={'CacheControl': 'max-age=3600', 'ContentType': 'text/html'})
+                             CacheControl='max-age=3600', ContentType='text/html')
 
         s3_client.upload_fileobj(buffer, bucket, f'img/{shortCode}',
-                             ExtraArgs={'CacheControl': 'max-age=86400', 'ContentType': 'image/jpeg'})
+                                 ExtraArgs={'CacheControl': 'max-age=86400', 'ContentType': 'image/jpeg'})
 
     response = {}
     response['statusCode'] = 301
@@ -76,9 +76,9 @@ def token_resize(buffer, size, debug=False):
 
     # crop the image to a square, saving the center bits
     if width > height:
-        img = img.crop(box=((width-height)/2, 0, ((width-height)/2)+height, height))
+        img = img.crop(box=((width - height) / 2, 0, ((width - height) / 2) + height, height))
     elif width < height:
-        img = img.crop(box=(0, (height-width)/2, width, ((height-width)/2)+width))
+        img = img.crop(box=(0, (height - width) / 2, width, ((height - width) / 2) + width))
 
     # resize the image up or down to size x size
     img = img.resize((size, size))
@@ -89,55 +89,56 @@ def token_resize(buffer, size, debug=False):
 
 
 def test():
-        taller = "https://i.pinimg.com/236x/8e/b3/30/8eb3305d2477ec223a8987a417bb1413.jpg"
-        wider = "https://images-na.ssl-images-amazon.com/images/I/816aY1-FCFL._SX500_.jpg"
-        trans = "https://i.imgur.com/3Gm6vW9.jpg"
-        size = 160
+    taller = "https://i.pinimg.com/236x/8e/b3/30/8eb3305d2477ec223a8987a417bb1413.jpg"
+    wider = "https://images-na.ssl-images-amazon.com/images/I/816aY1-FCFL._SX500_.jpg"
+    trans = "https://i.imgur.com/3Gm6vW9.jpg"
+    size = 160
 
-        with io.BytesIO() as buffer:
-            download_img(buffer, taller)
-            print(f"original buffer size: {len(buffer.getvalue())}")
-            with open("/tmp/taller.jpg", "wb") as f:
-                f.write(buffer.getvalue())
-            token_resize(
-                buffer,
-                size=size,
-                debug=True
-            )
-            buffer.seek(0)
-            print(f"final buffer size: {len(buffer.getvalue())}")
-            with open("/tmp/taller.png", "wb") as f:
-                f.write(buffer.getvalue())
+    with io.BytesIO() as buffer:
+        download_img(buffer, taller)
+        print(f"original buffer size: {len(buffer.getvalue())}")
+        with open("/tmp/taller.jpg", "wb") as f:
+            f.write(buffer.getvalue())
+        token_resize(
+            buffer,
+            size=size,
+            debug=True
+        )
+        buffer.seek(0)
+        print(f"final buffer size: {len(buffer.getvalue())}")
+        with open("/tmp/taller.png", "wb") as f:
+            f.write(buffer.getvalue())
 
-        with io.BytesIO() as buffer:
-            download_img(buffer, wider)
-            print(f"original buffer size: {len(buffer.getvalue())}")
-            with open("/tmp/wider.jpg", "wb") as f:
-                f.write(buffer.getvalue())
-            token_resize(
-                buffer,
-                size=size,
-                debug=True
-            )
-            buffer.seek(0)
-            print(f"final buffer size: {len(buffer.getvalue())}")
-            with open("/tmp/wider.png", "wb") as f:
-                f.write(buffer.getvalue())
+    with io.BytesIO() as buffer:
+        download_img(buffer, wider)
+        print(f"original buffer size: {len(buffer.getvalue())}")
+        with open("/tmp/wider.jpg", "wb") as f:
+            f.write(buffer.getvalue())
+        token_resize(
+            buffer,
+            size=size,
+            debug=True
+        )
+        buffer.seek(0)
+        print(f"final buffer size: {len(buffer.getvalue())}")
+        with open("/tmp/wider.png", "wb") as f:
+            f.write(buffer.getvalue())
 
-        with io.BytesIO() as buffer:
-            download_img(buffer, trans)
-            print(f"original buffer size: {len(buffer.getvalue())}")
-            with open("/tmp/trans.jpg", "wb") as f:
-                f.write(buffer.getvalue())
-            token_resize(
-                buffer,
-                size=size,
-                debug=True
-            )
-            buffer.seek(0)
-            print(f"final buffer size: {len(buffer.getvalue())}")
-            with open("/tmp/trans.png", "wb") as f:
-                f.write(buffer.getvalue())
+    with io.BytesIO() as buffer:
+        download_img(buffer, trans)
+        print(f"original buffer size: {len(buffer.getvalue())}")
+        with open("/tmp/trans.jpg", "wb") as f:
+            f.write(buffer.getvalue())
+        token_resize(
+            buffer,
+            size=size,
+            debug=True
+        )
+        buffer.seek(0)
+        print(f"final buffer size: {len(buffer.getvalue())}")
+        with open("/tmp/trans.png", "wb") as f:
+            f.write(buffer.getvalue())
+
 
 if __name__ == '__main__':
     test()
