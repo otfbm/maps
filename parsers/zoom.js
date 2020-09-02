0f28b762650f9a349e4a083cd1a200091004ba91
Matt <matthew.spence@gmail.com>
2020-09-02 16:09:04 +1200

    Zoom option was being picked up incorrectly.

--- parsers/zoom.js
+++ parsers/zoom.js
@@ -1,8 +1,8 @@
 module.exports = class ZoomParser {
     parse(obj) {
-        let match = obj.str.match(/[0-9](\.[0-9]{1,3})?|\.[0-9]{1,3}/);
+        let match = obj.str.match(/^@([0-9](\.[0-9]{1,3})?|\.[0-9]{1,3})/);
         if (match) {
-            let zoom = Number(match[0]);
+            let zoom = Number(match[1]);
             return zoom <= 3 ? zoom : 3;
         }
         return false;
