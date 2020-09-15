47ab330e90e5cf8dfd48d83c65c1b0aa130c99be
Matt <matthew.spence@gmail.com>
2020-09-16 10:47:08 +1200

    put the white edge back for image tokens

--- overlays/token/template.js
+++ overlays/token/template.js
@@ -14,43 +14,40 @@ module.exports = ({
   const radius = (size + 1) / 2 - 3;
   const xy = size < gridsize ? (gridsize + 1) / 2 : (size + 1) / 2;
   const imageTL = size < gridsize ? (gridsize - size) / 2 : 0;
+  let tokenEdgeColour = '#07031a';
+
+  // inner token edge
+  ctx.beginPath();
+  ctx.arc(xy, xy, radius - whitelineModifier, 0, Math.PI * 2);
+  ctx.strokeStyle = '#f4f6ff';
+  ctx.lineWidth = whitelineModifier;
+  ctx.stroke();
 
   if (image && size >= 40) {
-    ctx.beginPath();
-    ctx.arc(xy, xy, radius - whitelineModifier, 0, Math.PI * 2);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = whitelineModifier + 3;
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(xy, xy, radius - whitelineModifier, 0, Math.PI * 2);
+    // fill with image
+    ctx.save()
     ctx.clip();
-
     const img = new Image();
     img.onload = () => ctx.drawImage(img, imageTL, imageTL, size, size);
     img.onerror = err => { throw new Error('Failed to load token image') };
     img.src = image;
+    ctx.restore()
+    tokenEdgeColour = color;
   } else {
-    ctx.beginPath();
-    ctx.arc(xy, xy, radius - whitelineModifier, 0, Math.PI * 2);
-    ctx.strokeStyle = '#f4f6ff';
-    ctx.lineWidth = whitelineModifier;
-    ctx.stroke();
+    // fill with colour and label
     ctx.fillStyle = color;
     ctx.fill();
-  }
-
-  ctx.beginPath();
-  ctx.arc(xy, xy, radius, 0, Math.PI * 2);
-  ctx.strokeStyle = '#07031a';
-  ctx.lineWidth = 1;
-  ctx.stroke();
-
-  if (!image || size < 40) {
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.font = `${fontsize}px ${font}`;
     ctx.fillStyle = fontcolor;
     ctx.fillText(label, xy, xy);
   }
+
+  // outer token edge
+  ctx.beginPath();
+  ctx.arc(xy, xy, radius, 0, Math.PI * 2);
+  ctx.strokeStyle = tokenEdgeColour;
+  ctx.lineWidth = 1;
+  ctx.stroke();
 }
