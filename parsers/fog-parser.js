2bf8d857fac62c82af312649d7ffa8e6c9a41bcf
Matt <matthew.spence@gmail.com>
2020-10-21 02:41:03 +1300

    Fog parser tidy up.

--- parsers/fog-parser.js
+++ parsers/fog-parser.js
@@ -1,15 +1,15 @@
 const CoordParser = require("./coord-parser.js");
 const FogEffect = require("../effects/fog-effect.js");
+const { false } = require("tap");
 
 module.exports = class FogParser {
   parse(str) {
     const reg = /\*F(([A-Z]{1,2}[0-9]{1,2})+)/i;
-    if (!reg.test(str))
-      return false;
-
     const matches = str.match(reg);
-    let coords = CoordParser.parseSet(matches[1]);
-
-    return new FogEffect({ startPt: coords[0], endPt: coords[1] });
+    if (matches) {
+      let coords = CoordParser.parseSet(matches[1]);
+      return new FogEffect({ startPt: coords[0], endPt: coords[1] });
+    }
+    return false;
   }
 }
