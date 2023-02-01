f2cba2ad0af4aa2846af4f60ad0240ca194af4cd
Patrick Burch <patpluspun@users.noreply.github.com>
2023-02-01 18:28:36 -0500

    Added color param to all objects and set fill on each svg to color param (#57)

--- overlays/explosion/index.js
+++ overlays/explosion/index.js
@@ -10,12 +10,12 @@ module.exports = class ExplosionOverlay {
   }
 
   render(cell) {
-    const width = cell.overlay.width;
-    const height = cell.overlay.height;
+    const { width, height, color } = cell.overlay;
 
     const svg = template({
       height,
       width,
+      color,
     });
     return `data:image/svg+xml;base64,${Buffer.from(svg).toString("base64")}`;
   }
