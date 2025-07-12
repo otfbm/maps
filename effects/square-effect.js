9a3065893e232f38a393705f173e411614aced89
Pat <patrick.burch@nextgen.coop>
2025-07-11 23:39:16 -0500

    Adding alpha transparency option for overlays and underlays, .1-.9 options

--- effects/square-effect.js
+++ effects/square-effect.js
@@ -1,19 +1,23 @@
 module.exports = class SquareEffect {
-  constructor({ width, length, colour, startPt, endPt, anchorTopLeft }) {
+  constructor({ width, length, colour, startPt, endPt, anchorTopLeft, alpha }) {
     this.colour = colour;
     this.width = width;
     this.length = length;
     this.startPt = startPt;
     this.endPt = endPt;
     this.anchorTopLeft = anchorTopLeft;
+    this.alpha = alpha;
   }
 
   draw(ctx, gridSize) {
-    let unit = gridSize / 5;    
+    let unit = gridSize / 5;
     ctx.save();
 
     if (this.anchorTopLeft) {
-      ctx.translate((this.startPt.x - 1) * gridSize, (this.startPt.y - 1) * gridSize);
+      ctx.translate(
+        (this.startPt.x - 1) * gridSize,
+        (this.startPt.y - 1) * gridSize
+      );
       ctx.beginPath();
       ctx.moveTo(0, this.width * unit);
       ctx.lineTo(this.length * unit, this.width * unit);
@@ -22,23 +26,30 @@ module.exports = class SquareEffect {
       ctx.lineTo(0, this.width * unit);
     } else {
       let halfGrid = gridSize / 2;
-      ctx.translate((this.startPt.x * gridSize) - halfGrid, (this.startPt.y * gridSize) - halfGrid);
-      let angle = Math.atan2(this.endPt.y - this.startPt.y, this.endPt.x - this.startPt.x)   
+      ctx.translate(
+        this.startPt.x * gridSize - halfGrid,
+        this.startPt.y * gridSize - halfGrid
+      );
+      let angle = Math.atan2(
+        this.endPt.y - this.startPt.y,
+        this.endPt.x - this.startPt.x
+      );
       ctx.rotate(angle);
       ctx.translate(halfGrid, 0);
       ctx.beginPath();
-      ctx.moveTo(0, this.width / 2 * unit);
-      ctx.lineTo(this.length * unit, this.width / 2 * unit);
-      ctx.lineTo(this.length * unit, this.width / -2 * unit);
-      ctx.lineTo(0, this.width / -2 * unit);
-      ctx.lineTo(0, this.width / 2 * unit);
+      ctx.moveTo(0, (this.width / 2) * unit);
+      ctx.lineTo(this.length * unit, (this.width / 2) * unit);
+      ctx.lineTo(this.length * unit, (this.width / -2) * unit);
+      ctx.lineTo(0, (this.width / -2) * unit);
+      ctx.lineTo(0, (this.width / 2) * unit);
     }
 
     ctx.strokeStyle = this.colour;
     ctx.lineWidth = 1;
     ctx.stroke();
+    ctx.globalAlpha = this.alpha;
     ctx.fillStyle = this.colour + "88";
     ctx.fill();
     ctx.restore();
   }
-}
+};
--- effects/triangle-effect.js
+++ effects/triangle-effect.js
@@ -1,31 +1,39 @@
 module.exports = class TriangleEffect {
-  constructor({ size, colour, startPt, endPt }) {
+  constructor({ size, colour, startPt, endPt, alpha }) {
     this.colour = colour;
     this.width = size;
     this.startPt = startPt;
     this.endPt = endPt;
+    this.alpha = alpha;
   }
 
   draw(ctx, gridSize) {
-    let angle = Math.atan2(this.endPt.y - this.startPt.y, this.endPt.x - this.startPt.x)
+    let angle = Math.atan2(
+      this.endPt.y - this.startPt.y,
+      this.endPt.x - this.startPt.x
+    );
     let length = this.width / 5;
     let halfGrid = gridSize / 2;
     ctx.save();
-    ctx.translate((this.startPt.x * gridSize) - halfGrid, (this.startPt.y * gridSize) - halfGrid);
+    ctx.translate(
+      this.startPt.x * gridSize - halfGrid,
+      this.startPt.y * gridSize - halfGrid
+    );
     ctx.rotate(angle);
     ctx.translate(halfGrid, 0);
-      
+
     ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(length * gridSize, length / 2 * gridSize );
-    ctx.lineTo(length * gridSize, length / -2 * gridSize ); 
-    ctx.lineTo(0, 0); 
+    ctx.moveTo(0, 0);
+    ctx.lineTo(length * gridSize, (length / 2) * gridSize);
+    ctx.lineTo(length * gridSize, (length / -2) * gridSize);
+    ctx.lineTo(0, 0);
 
     ctx.strokeStyle = this.colour;
     ctx.lineWidth = 1;
     ctx.stroke();
+    ctx.globalAlpha = this.alpha;
     ctx.fillStyle = this.colour + "88";
     ctx.fill();
     ctx.restore();
   }
-}
+};
